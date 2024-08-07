e316d8097d2f398d7cb0b5283d3a0d5fe8876040
nanako1106 <nanakai626@gmail.com>
2024-08-07 13:24:05 +0900

    サインイン画面での表示修正

--- frontend/src/app/staff/layout.tsx
+++ frontend/src/app/staff/layout.tsx
@@ -1,14 +1,20 @@
 "use client";
 
 import React from "react";
+import { usePathname } from "next/navigation";
 import Header from "../../components/header";
 import MenuBar from "../../components/menuBar";
 
 const StaffLayout = ({ children }: { children: React.ReactNode }) => {
+  const pathname = usePathname();
+
+  // 現在のパスが /staff/signin で始まる場合は MenuBar を非表示にする
+  const showMenuBar = !pathname.startsWith("/staff/signin");
+
   return (
     <div className="flex h-screen flex-col bg-background">
       <Header />
-      <MenuBar />
+      {showMenuBar && <MenuBar />}
       <div className="flex flex-1">
         <div className="p-4 w-full">{children}</div>
       </div>
