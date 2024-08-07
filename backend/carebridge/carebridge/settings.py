0cf81d147c427fcf2dc4c398c09e04258ea051a2
amihasegawa <iyonagamy@gmail.com>
2024-08-07 14:50:31 +0900

    psycopg2-binary追加

--- backend/carebridge/carebridge/settings.py
+++ backend/carebridge/carebridge/settings.py
@@ -1,24 +1,20 @@
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+# .envファイルを読み込み
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
-
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '0.0.0.0,localhost,127.0.0.1').split(',')
 
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
