3e5c5c639968fbd993ee73d08c1325dd8e52de3e
amihasegawa <iyonagamy@gmail.com>
2024-08-12 13:36:51 +0900

    staffs_DB疎通_get

--- backend/carebridge/carebridge/urls.py
+++ backend/carebridge/carebridge/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('caredata.urls')), # ケア情報
     path('api/', include('users.urls')), # ユーザー情報
+    path('api/', include('staffs.urls')), # スタッフ情報
     path('api/', include('logout.urls')),  # logoutのエンドポイントを追加
 ]
