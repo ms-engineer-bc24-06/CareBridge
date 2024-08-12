7c74cb070cebd953605e7bcb779da4a4a4522a3f
amihasegawa <iyonagamy@gmail.com>
2024-08-12 14:12:24 +0900

    facilities_DB疎通_get

--- backend/carebridge/carebridge/urls.py
+++ backend/carebridge/carebridge/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('api/', include('caredata.urls')), # ケア情報
     path('api/', include('users.urls')), # ユーザー情報
     path('api/', include('staffs.urls')), # スタッフ情報
+    path('api/', include('facilities.urls')), # 施設情報
     path('api/', include('logout.urls')),  # logoutのエンドポイントを追加
 ]
--- backend/carebridge/facilities/urls.py
+++ backend/carebridge/facilities/urls.py
@@ -4,7 +4,7 @@ from .views import get_facilities, get_facility, create_facility, update_facilit
 urlpatterns = [
     path('facilities/', get_facilities, name='get_facilities'),
     path('facilities/<int:id>/', get_facility, name='get_facility'),
-    path('facilities/', create_facility, name='create_facility'),
-    path('facilities/<int:id>/', update_facility, name='update_facility'),
-    path('facilities/<int:id>/', delete_facility, name='delete_facility'),
+    path('facilities/create/', create_facility, name='create_facility'),
+    path('facilities/update/<int:id>/', update_facility, name='update_facility'),
+    path('facilities/delete/<int:id>/', delete_facility, name='delete_facility'),
 ]
