52a0de771ab82bab5a988397f13ba373d4b0cacd
amihasegawa <iyonagamy@gmail.com>
2024-08-13 10:49:48 +0900

    .env-compose追加

--- backend/carebridge/carebridge/urls.py
+++ backend/carebridge/carebridge/urls.py
@@ -6,6 +6,9 @@ urlpatterns = [
     path('api/', include('caredata.urls')), # ケア情報
     path('api/', include('users.urls')), # ユーザー情報
     path('api/', include('staffs.urls')), # スタッフ情報
+<<<<<<< HEAD
     path('api/', include('facilities.urls')), # 施設情報
+=======
+>>>>>>> 3e5c5c6 (staffs_DB疎通_get)
     path('api/', include('logout.urls')),  # logoutのエンドポイントを追加
 ]
