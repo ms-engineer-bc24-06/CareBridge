26c0342aacf1f3a6f0179b8b2d374c8b6b1390e5
nanako1106 <nanakai626@gmail.com>
2024-08-17 11:53:11 +0900

    backendのstaffs修正

--- backend/carebridge/staffs/urls.py
+++ backend/carebridge/staffs/urls.py
@@ -4,7 +4,7 @@ from .views import get_staffs, get_staff, create_staff, update_staff, delete_sta
 urlpatterns = [
     path('staffs/', get_staffs, name='get_staffs'),
     path('staffs/<uuid:uuid>/', get_staff, name='get_staff'),
-    path('staffs/', create_staff, name='create_staff'),
-    path('staffs/<uuid:uuid>/', update_staff, name='update_staff'),
-    path('staffs/<uuid:uuid>/', delete_staff, name='delete_staff'),
+    path('staffs/create/', create_staff, name='create_staff'),
+    path('staffs/<uuid:uuid>/update/', update_staff, name='update_staff'),
+    path('staffs/<uuid:uuid>/delete/', delete_staff, name='delete_staff'),
 ]
