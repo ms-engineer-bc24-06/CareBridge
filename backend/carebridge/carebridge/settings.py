849ea7864ae785a4765e09f938b65d79f6490ba2
‘harukaiinuma’ <h.iinuma23@gmail.com>
2024-08-22 13:56:38 +0900

    settings.py変更

--- backend/carebridge/carebridge/settings.py
+++ backend/carebridge/carebridge/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',  # 最初の方に配置
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -42,8 +43,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
+
 
 ROOT_URLCONF = 'carebridge.urls'
 
@@ -81,8 +82,8 @@ DATABASES = {
 }
 
 # CORSとCSRF設定の追加
-CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000',
+CORS_ALLOWED_ORIGINS= [
+    'http://localhost:3000'
 ]
 
 CSRF_TRUSTED_ORIGINS = [
