5c0a46a7b471f67a28145bec4bedca9ceda8c193
amihasegawa <iyonagamy@gmail.com>
2024-09-03 15:24:48 +0900

    admin/facilities_PUT_OK

--- backend/carebridge/facilities/urls.py
+++ backend/carebridge/facilities/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('facilities/', get_facilities, name='get_facilities'),
     path('facilities/<int:id>/', get_facility, name='get_facility'),
     path('facilities/create/', create_facility, name='create_facility'),
-    path('facilities/update/<int:id>/', update_facility, name='update_facility'),
+    path('facilities/<int:id>/update/', update_facility, name='update_facility'),
     path('facilities/delete/<int:id>/', delete_facility, name='delete_facility'),
     path('staffs/get_staff_facility_id/', get_staff_facility_id, name='get_staff_facility_id'),
 ]
