14ffaa91a1a6708eb2a0c7e8d9d4c70b9291ce20
‘harukaiinuma’ <h.iinuma23@gmail.com>
2024-09-09 14:07:37 +0900

    トップページエラー解消

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,19 +1,3 @@
-// "use client";
-
-// import React from "react";
-// import SignInPage from "./staff/signin/page";
-
-// const Home: React.FC = () => {
-//   return (
-//     <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-gray-100">
-//       {/* `SignInPage` コンポーネントを表示 */}
-//       <SignInPage />
-//     </main>
-//   );
-// };
-
-// export default Home;
-// src/app/page.tsx
 "use client";
 
 import React from "react";
@@ -22,8 +6,11 @@ import Link from "next/link";
 const Home: React.FC = () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-gray-100">
-      <Link href="/staff/signin">
-        <a className="text-blue-500 underline">Go to Sign In Page</a>
+      <Link legacyBehavior href="/staff/signin">
+        <a className="text-blue-500 underline">介護職員ログイン</a>
+      </Link>
+      <Link legacyBehavior href="/user/signin">
+        <a className="text-blue-500 underline">ご家族ログイン</a>
       </Link>
     </main>
   );
